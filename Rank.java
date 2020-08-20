e6d5876ddf530f2c2353a26ce6c12bb6f12feca7
Abhishek2798 <35943152+Abhishek2798@users.noreply.github.com>
2020-08-20 15:03:30 +0530

    Java Assignment 2

---
+++ Rank.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Rank
+{
+public static void transform(int[] arr)
+{
+Map<Integer, Integer> map = new TreeMap<>();
+
+for (int i = 0; i < arr.length; i++) {
+map.put(arr[i], i);
+}
+
+int rank = 1;
+
+for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+arr[entry.getValue()] = rank++;
+}
+}
+
+public static void main(String[] args)
+{
+int[] A = { 13, 6, 7, 11, 9, 20, 1 };
+
+transform(A);
+
+System.out.println(Arrays.toString(A));
+}
+}
